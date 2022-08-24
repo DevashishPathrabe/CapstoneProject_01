ffe2183bc8726eddf1d50e645f0c82f3e4aa8586
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-24 16:08:39 +0530

    Update QuestionService.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/service/QuestionService.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/service/QuestionService.java
@@ -7,12 +7,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.wipro.cp.doconnect.dto.AnswerResponseDTO;
 import com.wipro.cp.doconnect.dto.QuestionRequestDTO;
 import com.wipro.cp.doconnect.dto.QuestionResponseDTO;
 import com.wipro.cp.doconnect.dto.QuestionUpdateDTO;
 import com.wipro.cp.doconnect.dto.StatusDTO;
-import com.wipro.cp.doconnect.entity.Answer;
 import com.wipro.cp.doconnect.entity.Question;
 import com.wipro.cp.doconnect.repository.QuestionRepository;
 
@@ -22,33 +20,12 @@ public class QuestionService {
 	@Autowired
 	private QuestionRepository questionRepository;
 	
-	private AnswerResponseDTO convertAnswerToAnswerResponseDTO(Answer answer) {
-		return new AnswerResponseDTO(answer.getId(), answer.getAnswer(), answer.getImages(), answer.getPostedBy(), answer.getPostedAt(), answer.getApprovedBy(), answer.getIsApproved());
-	}
-	
-	private List<AnswerResponseDTO> convertAnswerListToAnswerResponseDTOList(List<Answer> answerList) {
-		return answerList.stream().map(answer -> convertAnswerToAnswerResponseDTO(answer)).collect(Collectors.toList());
-	}
-	
 	private QuestionResponseDTO convertQuestionToQuestionResponseDTO(Question question) {
-		return new QuestionResponseDTO(question.getId(), question.getQuestion(), question.getTopic(), question.getImages(), question.getPostedBy(), question.getPostedAt(), question.getApprovedBy(), question.getIsApproved(), convertAnswerListToAnswerResponseDTOList(question.getAnswers()));
+		return new QuestionResponseDTO(question.getId(), question.getQuestion(), question.getTopic(), question.getImages(), question.getPostedBy(), question.getPostedAt(), question.getApprovedBy(), question.getIsApproved());
 	}
 	
 	private List<QuestionResponseDTO> convertQuestionListToQuestionResponseDTOList(List<Question> questionList) {
 		return questionList.stream().map(question -> convertQuestionToQuestionResponseDTO(question)).collect(Collectors.toList());
-	}
-	
-	private boolean checkIfAnswerIsApproved(Answer answer) {
-		return answer.getIsApproved();
-	}
-	
-	private QuestionResponseDTO convertQuestionToQuestionResponseDTOWithApprovedAnswers(Question question) {
-		List<AnswerResponseDTO> approvedAnswerResponseDTOList = convertAnswerListToAnswerResponseDTOList(question.getAnswers().stream().filter(answer -> checkIfAnswerIsApproved(answer)).collect(Collectors.toList()));
-		return new QuestionResponseDTO(question.getId(), question.getQuestion(), question.getTopic(), question.getImages(), question.getPostedBy(), question.getPostedAt(), question.getApprovedBy(), question.getIsApproved(), approvedAnswerResponseDTOList);
-	}
-	
-	private List<QuestionResponseDTO> convertQuestionListToQuestionResponseDTOListWithApprovedAnswers(List<Question> questionList) {
-		return questionList.stream().map(question -> convertQuestionToQuestionResponseDTOWithApprovedAnswers(question)).collect(Collectors.toList());
 	}
 	
 	public StatusDTO<List<QuestionResponseDTO>> getAllQuestions(String status, String search) {
@@ -57,7 +34,7 @@ public class QuestionService {
 				return new StatusDTO<List<QuestionResponseDTO>>("", true, convertQuestionListToQuestionResponseDTOList(questionRepository.findAll()));
 			}
 			else if (status.equalsIgnoreCase("approved")) {
-				return new StatusDTO<List<QuestionResponseDTO>>("", true, convertQuestionListToQuestionResponseDTOListWithApprovedAnswers(questionRepository.findByIsApprovedTrue()));
+				return new StatusDTO<List<QuestionResponseDTO>>("", true, convertQuestionListToQuestionResponseDTOList(questionRepository.findByIsApprovedTrue()));
 			}
 			else if (status.equalsIgnoreCase("unapproved")) {
 				return new StatusDTO<List<QuestionResponseDTO>>("", true, convertQuestionListToQuestionResponseDTOList(questionRepository.findByIsApprovedFalse()));
@@ -68,7 +45,7 @@ public class QuestionService {
 		}
 		else {
 			if (status.equalsIgnoreCase("approved")) {
-				return new StatusDTO<List<QuestionResponseDTO>>("", true, convertQuestionListToQuestionResponseDTOListWithApprovedAnswers(questionRepository.findByQuestionContainingIgnoreCaseAndIsApprovedTrue(search)));
+				return new StatusDTO<List<QuestionResponseDTO>>("", true, convertQuestionListToQuestionResponseDTOList(questionRepository.findByQuestionContainingIgnoreCaseAndIsApprovedTrue(search)));
 			}
 			else {
 				return new StatusDTO<List<QuestionResponseDTO>>("Question search only works with 'approved' status.", false, null);
@@ -78,13 +55,14 @@ public class QuestionService {
 	
 	public StatusDTO<QuestionResponseDTO> createQuestion(QuestionRequestDTO questionRequestDTO, String postedBy) {
 		Question question = new Question(questionRequestDTO.getQuestion(), questionRequestDTO.getTopic(), questionRequestDTO.getImages(), postedBy);
+		// TODO: Send email to Admin that question is created
 		return new StatusDTO<QuestionResponseDTO>("", true, convertQuestionToQuestionResponseDTO(questionRepository.save(question)));
 	}
 	
-	public StatusDTO<QuestionResponseDTO> updateQuestion(QuestionUpdateDTO questionUpdateDTO, Long id, String approvedBy) {
-		Optional<Question> optionalQuestion = questionRepository.findById(id);
+	public StatusDTO<QuestionResponseDTO> updateQuestion(QuestionUpdateDTO questionUpdateDTO, Long questionId, String approvedBy) {
+		Optional<Question> optionalQuestion = questionRepository.findById(questionId);
 		if (optionalQuestion.isEmpty()) {
-			return new StatusDTO<QuestionResponseDTO>("User with ID " + id + " does not exist.", false, null);
+			return new StatusDTO<QuestionResponseDTO>("Question with ID " + questionId + " does not exist.", false, null);
 		}
 		Question question = optionalQuestion.get();
 		question.setIsApproved(questionUpdateDTO.getIsApproved());
@@ -92,12 +70,12 @@ public class QuestionService {
 		return new StatusDTO<QuestionResponseDTO>("", true, convertQuestionToQuestionResponseDTO(questionRepository.save(question)));
 	}
 	
-	public boolean deleteQuestionById(Long id) {
-		Optional<Question> optionalQuestion = questionRepository.findById(id);
+	public boolean deleteQuestionById(Long questionId) {
+		Optional<Question> optionalQuestion = questionRepository.findById(questionId);
 		if (optionalQuestion.isEmpty()) {
 			return false;
 		}
-		questionRepository.deleteById(id);
+		questionRepository.deleteById(questionId);
 		return true;
 	}
 
