9a80d59c17ad4bd4b24384ccc6e37aaf13507f60
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-24 17:16:05 +0530

    Update AnswerController.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/controller/AnswerController.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/controller/AnswerController.java
@@ -3,16 +3,23 @@ package com.wipro.cp.doconnect.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wipro.cp.doconnect.dto.AnswerRequestDTO;
 import com.wipro.cp.doconnect.dto.AnswerResponseDTO;
+import com.wipro.cp.doconnect.dto.AnswerUpdateDTO;
 import com.wipro.cp.doconnect.dto.StatusDTO;
 import com.wipro.cp.doconnect.service.AnswerService;
 
@@ -45,6 +52,28 @@ public class AnswerController {
 		StatusDTO<List<AnswerResponseDTO>> answerResponseDTOStatus = answerService.getAllAnswersForQuestionId(questionId, answerStatus);
 		if (!answerResponseDTOStatus.getIsValid()) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(answerResponseDTOStatus.getStatusMessage());
+		}
+		return ResponseEntity.ok(answerResponseDTOStatus.getObject());
+	}
+	
+	@PostMapping("/questions/{questionId}/answers")
+	public ResponseEntity<?> createAnswerForQuestionId(@PathVariable(value="questionId") Long questionId, @Valid @RequestBody AnswerRequestDTO answerRequestDTO) {
+		// TODO: Read the postedBy value from Authorization Header
+		String postedBy = "";
+		StatusDTO<AnswerResponseDTO> answerResponseDTOStatus = answerService.createAnswerForQuestionId(questionId, answerRequestDTO, postedBy);
+		if (!answerResponseDTOStatus.getIsValid()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(answerResponseDTOStatus.getStatusMessage());
+		}
+		return ResponseEntity.status(HttpStatus.CREATED).body(answerResponseDTOStatus.getObject());
+	}
+	
+	@PutMapping("/questions/{questionId}/answers/{answerId}")
+	public ResponseEntity<?> updateAnswerForQuestionId(@Valid @RequestBody AnswerUpdateDTO answerUpdateDTO, @PathVariable(value="questionId") Long questionId, @PathVariable(value="answerId") Long answerId) {
+		// TODO: Read the approvedBy value from Authorization Header
+		String approvedBy = "";
+		StatusDTO<AnswerResponseDTO> answerResponseDTOStatus = answerService.updateAnswerForQuestionId(questionId, answerId, answerUpdateDTO, approvedBy);
+		if (!answerResponseDTOStatus.getIsValid()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(answerResponseDTOStatus.getStatusMessage());
 		}
 		return ResponseEntity.ok(answerResponseDTOStatus.getObject());
 	}
