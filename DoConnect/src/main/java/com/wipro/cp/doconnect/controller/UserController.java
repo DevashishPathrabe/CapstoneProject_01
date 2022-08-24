5672716424be4b1a658be948e051c74eaa4d8b77
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-25 01:09:18 +0530

    Update UserController.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/controller/UserController.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wipro.cp.doconnect.dto.StatusDTO;
@@ -25,6 +26,7 @@ import com.wipro.cp.doconnect.service.UserServiceImp;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/v1")
 public class UserController {
 	
 	@Autowired
