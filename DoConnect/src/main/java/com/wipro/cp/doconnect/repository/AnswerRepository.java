2e125c420a8278121bf6fb18fee60c0d3beb7bb4
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-24 16:08:44 +0530

    Update AnswerRepository.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/repository/AnswerRepository.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/repository/AnswerRepository.java
@@ -1,5 +1,8 @@
 package com.wipro.cp.doconnect.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,8 @@ import com.wipro.cp.doconnect.entity.Answer;
 
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+	
+	List<Answer> findByQuestionId(Long questionId);
+	Optional<Answer> findByIdAndQuestionId(Long answerId, Long questionId);
 
 }
