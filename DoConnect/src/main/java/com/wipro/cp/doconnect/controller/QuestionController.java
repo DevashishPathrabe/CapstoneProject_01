46764c684a3f93ba3429ed667ca503efe490b256
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-25 01:09:20 +0530

    Update QuestionController.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/controller/QuestionController.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/controller/QuestionController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,7 @@ import com.wipro.cp.doconnect.service.QuestionServiceImp;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/v1")
 public class QuestionController {
 	
 	@Autowired
