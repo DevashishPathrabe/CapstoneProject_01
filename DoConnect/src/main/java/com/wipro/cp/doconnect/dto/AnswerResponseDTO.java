97fc05990274767c9d09113d965354ed4bdb58d6
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-24 15:22:13 +0530

    Create AnswerResponseDTO.java

---
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/dto/AnswerResponseDTO.java
@@ -0,0 +1,60 @@
+package com.wipro.cp.doconnect.dto;
+
+import java.util.Date;
+import java.util.List;
+
+public class AnswerResponseDTO {
+
+	private long id;
+	private String answer;
+	private List<String> images;
+	private String postedBy;
+	private Date postedAt;
+	private String approvedBy;
+	private boolean isApproved;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getAnswer() {
+		return answer;
+	}
+
+	public List<String> getImages() {
+		return images;
+	}
+
+	public String getPostedBy() {
+		return postedBy;
+	}
+
+	public Date getPostedAt() {
+		return postedAt;
+	}
+
+	public String getApprovedBy() {
+		return approvedBy;
+	}
+
+	public boolean getIsApproved() {
+		return isApproved;
+	}
+
+	public AnswerResponseDTO(long id, String answer, List<String> images, String postedBy, Date postedAt,
+			String approvedBy, boolean isApproved) {
+		super();
+		this.id = id;
+		this.answer = answer;
+		this.images = images;
+		this.postedBy = postedBy;
+		this.postedAt = postedAt;
+		this.approvedBy = approvedBy;
+		this.isApproved = isApproved;
+	}
+
+	public AnswerResponseDTO() {
+		super();
+	}
+
+}
