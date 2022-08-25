e78f6d708ad4c56efdf92372efc630cc765c84c4
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-26 01:04:37 +0530

    Update Utilities.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/util/Utilities.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/util/Utilities.java
@@ -3,15 +3,23 @@ package com.wipro.cp.doconnect.util;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.wipro.cp.doconnect.component.JwtTokenUtilityComponent;
 import com.wipro.cp.doconnect.dto.AnswerResponseDTO;
 import com.wipro.cp.doconnect.dto.QuestionResponseDTO;
 import com.wipro.cp.doconnect.entity.Answer;
 import com.wipro.cp.doconnect.entity.Question;
 import com.wipro.cp.doconnect.entity.User;
 
+@Component
 public class Utilities {
 	
-	public static String[] getUserEmails(List<User> userList) {
+	@Autowired
+	private JwtTokenUtilityComponent jwtTokenUtilityComponent;
+	
+	public String[] getUserEmails(List<User> userList) {
 		String[] array = new String[userList.size()];
 		for (int i = 0; i < userList.size(); i++) {
 			array[i] = userList.get(i).getEmail();
@@ -19,20 +27,25 @@ public class Utilities {
 		return array;
 	}
 	
-	public static QuestionResponseDTO convertQuestionToQuestionResponseDTO(Question question) {
+	public QuestionResponseDTO convertQuestionToQuestionResponseDTO(Question question) {
 		return new QuestionResponseDTO(question.getId(), question.getQuestion(), question.getTopic(), question.getImages(), question.getPostedBy(), question.getPostedAt(), question.getApprovedBy(), question.getIsApproved());
 	}
 	
-	public static List<QuestionResponseDTO> convertQuestionListToQuestionResponseDTOList(List<Question> questionList) {
+	public List<QuestionResponseDTO> convertQuestionListToQuestionResponseDTOList(List<Question> questionList) {
 		return questionList.stream().map(question -> convertQuestionToQuestionResponseDTO(question)).collect(Collectors.toList());
 	}
 	
-	public static AnswerResponseDTO convertAnswerToAnswerResponseDTO(Answer answer) {
-		return new AnswerResponseDTO(answer.getId(), answer.getAnswer(), answer.getImages(), answer.getPostedBy(), answer.getPostedAt(), answer.getApprovedBy(), answer.getIsApproved(), Utilities.convertQuestionToQuestionResponseDTO(answer.getQuestion()));
+	public AnswerResponseDTO convertAnswerToAnswerResponseDTO(Answer answer) {
+		return new AnswerResponseDTO(answer.getId(), answer.getAnswer(), answer.getImages(), answer.getPostedBy(), answer.getPostedAt(), answer.getApprovedBy(), answer.getIsApproved(), convertQuestionToQuestionResponseDTO(answer.getQuestion()));
 	}
 	
-	public static List<AnswerResponseDTO> convertAnswerListToAnswerResponseDTOList(List<Answer> answerList) {
+	public List<AnswerResponseDTO> convertAnswerListToAnswerResponseDTOList(List<Answer> answerList) {
 		return answerList.stream().map(answer -> convertAnswerToAnswerResponseDTO(answer)).collect(Collectors.toList());
+	}
+	
+	public String getUsernameFromAuthorizationHeader(String authorizationHeader) {
+		String jwtToken = authorizationHeader.substring(7);
+		return jwtTokenUtilityComponent.getUsernameFromToken(jwtToken);
 	}
 
 }
