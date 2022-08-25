a054b36061fdb9f488d4ccbf721079d8c6ac70b0
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-26 01:07:02 +0530

    Update AnswerController.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/controller/AnswerController.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/controller/AnswerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,7 @@ import com.wipro.cp.doconnect.dto.AnswerResponseDTO;
 import com.wipro.cp.doconnect.dto.AnswerUpdateDTO;
 import com.wipro.cp.doconnect.dto.StatusDTO;
 import com.wipro.cp.doconnect.service.AnswerServiceImp;
+import com.wipro.cp.doconnect.util.Utilities;
 
 @RestController
 @CrossOrigin
@@ -32,6 +35,9 @@ public class AnswerController {
 	
 	@Autowired
 	AnswerServiceImp answerServiceImp;
+	
+	@Autowired
+	private Utilities utilities;
 	
 	@GetMapping("/answers")
 	public ResponseEntity<?> getAllAnswers(@RequestParam(name="status") Optional<String> optionalStatus) {
@@ -60,9 +66,8 @@ public class AnswerController {
 	}
 	
 	@PostMapping("/questions/{questionId}/answers")
-	public ResponseEntity<?> createAnswerForQuestionId(@PathVariable(value="questionId") Long questionId, @Valid @RequestBody AnswerRequestDTO answerRequestDTO) {
-		// TODO: Read the postedBy value from Authorization Header
-		String postedBy = "Dummy";
+	public ResponseEntity<?> createAnswerForQuestionId(@PathVariable(value="questionId") Long questionId, @Valid @RequestBody AnswerRequestDTO answerRequestDTO, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+		String postedBy = utilities.getUsernameFromAuthorizationHeader(authorizationHeader);
 		StatusDTO<AnswerResponseDTO> answerResponseDTOStatus = answerServiceImp.createAnswerForQuestionId(questionId, answerRequestDTO, postedBy);
 		if (!answerResponseDTOStatus.getIsValid()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(answerResponseDTOStatus.getStatusMessage());
@@ -71,9 +76,8 @@ public class AnswerController {
 	}
 	
 	@PutMapping("/questions/{questionId}/answers/{answerId}")
-	public ResponseEntity<?> updateAnswerForQuestionId(@Valid @RequestBody AnswerUpdateDTO answerUpdateDTO, @PathVariable(value="questionId") Long questionId, @PathVariable(value="answerId") Long answerId) {
-		// TODO: Read the approvedBy value from Authorization Header
-		String approvedBy = "";
+	public ResponseEntity<?> updateAnswerForQuestionId(@Valid @RequestBody AnswerUpdateDTO answerUpdateDTO, @PathVariable(value="questionId") Long questionId, @PathVariable(value="answerId") Long answerId, @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+		String approvedBy = utilities.getUsernameFromAuthorizationHeader(authorizationHeader);
 		StatusDTO<AnswerResponseDTO> answerResponseDTOStatus = answerServiceImp.updateAnswerForQuestionId(questionId, answerId, answerUpdateDTO, approvedBy);
 		if (!answerResponseDTOStatus.getIsValid()) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(answerResponseDTOStatus.getStatusMessage());
