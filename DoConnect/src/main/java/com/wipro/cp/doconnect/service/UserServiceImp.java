1967e09c55b3f4a38eaeca887ff92cec51ec5d38
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-26 01:04:43 +0530

    Update UserServiceImp.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/service/UserServiceImp.java
+++ DoConnect/src/main/java/com/wipro/cp/doconnect/service/UserServiceImp.java
@@ -1,11 +1,15 @@
 package com.wipro.cp.doconnect.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.wipro.cp.doconnect.dto.StatusDTO;
@@ -16,13 +20,13 @@ import com.wipro.cp.doconnect.entity.User;
 import com.wipro.cp.doconnect.repository.UserRepository;
 
 @Service
-public class UserServiceImp implements IUserService {
+public class UserServiceImp implements IUserService, UserDetailsService {
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-//	@Autowired
-//	private PasswordEncoder bcryptEncoder;
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
 	
 	private UserResponseDTO convertUserToUserResponseDTO(User user) {
 		return new UserResponseDTO(user.getId(), user.getUsername(), user.getName(), user.getEmail(), user.getIsAdmin());
@@ -38,6 +42,16 @@ public class UserServiceImp implements IUserService {
 	public StatusDTO<UserResponseDTO> getUserByUsername(String username) {
 		return convertOptionalUserToStatusDTOUserResponseDTO(userRepository.findByUsername(username), "User with username " + username + " does not exist.");
 	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> optionalUser = userRepository.findByUsername(username);
+		if (optionalUser.isEmpty()) {
+			throw new UsernameNotFoundException("User not found with username: " + username);
+		}
+		User user = optionalUser.get();
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+	}
 
 	@Override
 	public StatusDTO<UserResponseDTO> createUser(UserRegisterDTO userRegisterDTO) {
@@ -50,7 +64,7 @@ public class UserServiceImp implements IUserService {
 		if (userRegisterDTO.getPassword().length() < 8) {
 			return new StatusDTO<UserResponseDTO>("Password should contain at least 8 characters.", false, null);
 		}
-		User user = new User(userRegisterDTO.getUsername(), userRegisterDTO.getName(), userRegisterDTO.getPassword(), userRegisterDTO.getEmail(), userRegisterDTO.getIsAdmin());
+		User user = new User(userRegisterDTO.getUsername(), userRegisterDTO.getName(), bcryptEncoder.encode(userRegisterDTO.getPassword()), userRegisterDTO.getEmail(), userRegisterDTO.getIsAdmin());
 		return new StatusDTO<UserResponseDTO>("", true, convertUserToUserResponseDTO(userRepository.save(user)));
 	}
 
