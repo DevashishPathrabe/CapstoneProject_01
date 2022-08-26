ec68395954dbfbd0e023d796f8f18cb2958ff453
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-26 15:47:10 +0530

    Create AuthorizationConfig.java

---
+++ do_connect_backend/DoConnect/src/main/java/com/wipro/cp/doconnect/config/AuthorizationConfig.java
@@ -0,0 +1,25 @@
+package com.wipro.cp.doconnect.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.wipro.cp.doconnect.interceptor.AuthorizationInterceptor;
+
+@Configuration
+@EnableWebMvc
+public class AuthorizationConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public AuthorizationInterceptor authorizationInterceptor() {
+	    return new AuthorizationInterceptor();
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authorizationInterceptor()).addPathPatterns("/**");
+	}
+
+}
