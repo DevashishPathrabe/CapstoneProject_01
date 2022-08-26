c21557379fa77ec317a36c0ac8bb5266855375df
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-26 15:43:31 +0530

    Delete IEmailService.java

--- DoConnect/src/main/java/com/wipro/cp/doconnect/service/IEmailService.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.cp.doconnect.service;
-
-import com.wipro.cp.doconnect.dto.EmailDTO;
-
-public interface IEmailService {
-	
-	boolean sendNotificationEmail(EmailDTO emailDTO);
-	
-}
