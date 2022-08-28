a73c3f5b2860af2898c2627b25de065890ad571a
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-28 17:23:35 +0530

    Update chat.component.ts

--- do_connect_frontend/src/app/chat/chat.component.ts
+++ do_connect_frontend/src/app/chat/chat.component.ts
@@ -1,37 +1,65 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ChatService } from '../service/chat.service';
+import { getCurrentUsername, isUserAdmin } from '../utils/util';
+
+const CHAT_REFRESH_FRQUENCY_IN_SECONDS = 5;
+
+interface ChatMessage {
+  message: string;
+  postedBy: string;
+  postedAt: Date;
+}
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css'],
 })
-export class ChatComponent implements OnInit {
-  constructor(private _chatservice: ChatService, private router: Router) {}
-  message: any;
-  data: any;
-  chatList: any;
+
+export class ChatComponent implements OnInit, OnDestroy {
+
+  message: string;
+  chatMessageList: ChatMessage[];
+  isUserAdmin: boolean;
+  interval: number | null = null;
+  currentUsername?: string = getCurrentUsername();
+
+  constructor(private _chatservice: ChatService) {
+    this.message = '';
+    this.chatMessageList = [];
+    this.isUserAdmin = isUserAdmin();
+  }
+
   ngOnInit(): void {
-    if (localStorage.getItem('isAdmin') === 'false') this.getChats();
+    if (!this.isUserAdmin) {
+      this.fetchChatMessages();
+      this.interval = window.setInterval(() => {
+        this.fetchChatMessages();
+      }, CHAT_REFRESH_FRQUENCY_IN_SECONDS * 1000);
+    }
   }
-  getChats() {
-    const seconds = 5;
-    setInterval(() => {
-      if (localStorage.getItem('isAdmin') === null) return;
-      this._chatservice.getChatList().subscribe({
-        next: (res) => (this.chatList = res),
-        error: (err) => console.warn(err),
-      });
-    }, seconds * 1000);
+
+  ngOnDestroy(): void {
+    if (!this.isUserAdmin && this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
   }
-  onSend() {
+
+  fetchChatMessages() {
+    this._chatservice.getChatMessagesList().subscribe({
+      next: (results) => (this.chatMessageList = results as ChatMessage[]),
+      error: (error) => console.error('Error while fetching chat messages list. Details: ', error),
+    });
+  }
+
+  onSendMessage() {
     this._chatservice.createChat({ message: this.message }).subscribe({
-      next: (res) => {
-        this.data = res;
+      next: (results) => {
         this.message = '';
+        this.chatMessageList = results as ChatMessage[];
       },
-      error: (err) => this.router.navigate(['/error/' + err.status]),
+      error: (error) => console.error('Error while sending chat message. Details: ', error),
     });
   }
 }
