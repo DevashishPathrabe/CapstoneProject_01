390a311dacc0715621e8986829dee4a2fbe1077f
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-28 17:23:09 +0530

    Update util.ts

--- do_connect_frontend/src/app/utils/util.ts
+++ do_connect_frontend/src/app/utils/util.ts
@@ -1,10 +1,41 @@
 import { HttpHeaders } from '@angular/common/http';
+import jwtDecode from 'jwt-decode';
+import { JWTCustomPayload } from '../constants/constants';
 
-export function getHeaders() {
+export const getHeaders = () => {
   const token = localStorage.getItem('token');
   const headers = new HttpHeaders({
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`,
   });
   return headers;
-}
+};
+
+export const isUserLoggedIn = () => {
+  const token = localStorage.getItem('token');
+  return (token !== null && token !== '' && token.split('.').length === 3);
+};
+
+export const isUserAdmin = () => {
+  if (isUserLoggedIn()) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const jwtData: JWTCustomPayload = jwtDecode(token);
+      return jwtData.isAdmin;
+    }
+    return false;
+  }
+  return false;
+};
+
+export const getCurrentUsername = () => {
+  if (isUserLoggedIn()) {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const jwtData: JWTCustomPayload = jwtDecode(token);
+      return jwtData.sub;
+    }
+    return undefined;
+  }
+  return undefined;
+};
