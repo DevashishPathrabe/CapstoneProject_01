13b93370bee695ff77eb795deaeae63b7f9f3ce3
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-28 17:23:33 +0530

    Update constants.ts

--- do_connect_frontend/src/app/constants/constants.ts
+++ do_connect_frontend/src/app/constants/constants.ts
@@ -1,1 +1,9 @@
+import { JwtPayload } from "jwt-decode";
+
 export const BASE_URL = 'http://localhost:9090/api/v1';
+
+export interface JWTCustomPayload extends JwtPayload {
+    isAdmin: boolean;
+    email: string;
+    name: string;
+}
