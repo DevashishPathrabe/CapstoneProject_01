a7cb917f1ec0333b56901aad59ecf4c52fd8da43
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-28 20:44:27 +0530

    Update login.component.ts

--- do_connect_frontend/src/app/login/login.component.ts
+++ do_connect_frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import jwtDecode from 'jwt-decode';
 import { JWTCustomPayload } from '../constants/constants';
@@ -10,33 +12,34 @@ import { UserService } from '../service/user.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  username = '';
-  password = '';
-  warning = '';
+
+  public loginForm !: FormGroup;
+
   constructor(private router: Router, private _userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+    });
+  }
 
-  onLogin() {
-    if (this.username === '' || this.password === '') {
-      this.warning = 'All field required';
-      return;
-    }
+  login() {
     this._userService
-      .login({ username: this.username, password: this.password })
+      .login({ username: this.loginForm.value.username, password: this.loginForm.value.password })
       .subscribe({
-        next: (res: any) => {
-          const jwtData: JWTCustomPayload = jwtDecode(res.token);
-          localStorage.setItem('token', res.token);
+        next: (result: any) => {
+          const token: string = result.token;
+          const jwtData: JWTCustomPayload = jwtDecode(token);
+          localStorage.setItem('token', token);
           if (jwtData.isAdmin) {
             this.router.navigate(['/dashboard']);
           } else {
             this.router.navigate(['/']);
           }
         },
-        error: (err) => {
-          alert('Invalid Credentials');
-          // this.router.navigate(['/error/' + err.status]);
+        error: (error: HttpErrorResponse) => {
+          alert('Invalid Credentials. Please try again.');
         },
       });
   }
