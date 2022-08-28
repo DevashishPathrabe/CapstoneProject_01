453e725428e6dbd788979e00a037d8fdf3aca699
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-29 00:13:38 +0530

    Update search.service.ts

--- do_connect_frontend/src/app/service/search.service.ts
+++ do_connect_frontend/src/app/service/search.service.ts
@@ -1,18 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BASE_URL } from '../constants/constants';
-import { getHeaders } from '../utils/util';
 
 @Injectable({
   providedIn: 'root',
 })
+
 export class SearchService {
   searchActive = 'none';
-  baseurl = BASE_URL
+
   constructor(private http:HttpClient) {}
 
   onOpen(data:any) {
-    const headers = getHeaders();
-    return this.http.get(this.baseurl+`/questions?search=${data}`, {headers});
+    return this.http.get(`${BASE_URL}/questions?search=${data}`);
   }
 }
