4def582c6c53e497135f2e2cdc8c59a2697b943b
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-28 17:23:24 +0530

    Update home.component.ts

--- do_connect_frontend/src/app/home/home.component.ts
+++ do_connect_frontend/src/app/home/home.component.ts
@@ -2,23 +2,26 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../service/user.service';
+import { isUserAdmin } from '../utils/util';
+
+const OPEN_CHAT_BUTTON_LABEL = 'Chat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
+
 export class HomeComponent implements OnInit {
   data: any;
   search: any;
   chatbox = 'none';
-  isAdmin: any;
-  chatButton = 'Open Chat';
+  isAdmin: boolean = isUserAdmin();
+  chatButton: string = OPEN_CHAT_BUTTON_LABEL;
+
   constructor(private _userService: UserService, private router: Router) {}
 
   ngOnInit(): void {
-    this.isAdmin = localStorage.getItem('isAdmin');
-    console.log(this.isAdmin === 'true');
     this._userService.getApprovedQuestions().subscribe({
       next: (res) => (this.data = res),
       error: (err) => this.router.navigate(['/error/' + err.status]),
@@ -27,7 +30,6 @@ export class HomeComponent implements OnInit {
 
   toggleChatBox() {
     this.chatbox = this.chatbox === 'block' ? 'none' : 'block';
-    this.chatButton = this.chatbox === 'block' ? 'Close Chat' : 'Open Chat';
     var elem: any = document.getElementById('msgbox');
     elem.scrollTop = elem.scrollHeight;
   }
