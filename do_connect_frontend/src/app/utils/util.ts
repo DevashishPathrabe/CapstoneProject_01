5302cbb83cf3aece8fc22e0229543887c555c477
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-29 00:13:45 +0530

    Update util.ts

--- do_connect_frontend/src/app/utils/util.ts
+++ do_connect_frontend/src/app/utils/util.ts
@@ -2,15 +2,6 @@ import { HttpHeaders } from '@angular/common/http';
 import jwtDecode from 'jwt-decode';
 import { JWTCustomPayload } from '../constants/constants';
 
-export const getHeaders = () => {
-  const token = localStorage.getItem('token');
-  const headers = new HttpHeaders({
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${token}`,
-  });
-  return headers;
-};
-
 export const isUserLoggedIn = () => {
   const token = localStorage.getItem('token');
   return (token !== null && token !== '' && token.split('.').length === 3);
