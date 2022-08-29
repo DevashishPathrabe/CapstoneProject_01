9f7b26a82bb01ddc62708c27e8506bc30b778788
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-29 22:26:51 +0530

    Create UserServiceImpTest.java

---
+++ do_connect_backend/DoConnect/src/test/java/com/wipro/cp/doconnect/service/UserServiceImpTest.java
@@ -0,0 +1,14 @@
+package com.wipro.cp.doconnect.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceImpTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
