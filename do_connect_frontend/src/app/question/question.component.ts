81f74df0f2cc9ced5967e727bbb49bec96dd79b0
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-29 17:42:13 +0530

    Update question.component.ts

--- do_connect_frontend/src/app/question/question.component.ts
+++ do_connect_frontend/src/app/question/question.component.ts
@@ -1,5 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Route, Router } from '@angular/router';
+import { AnswerType, BASE_URL, QuestionType } from '../constants/constants';
 import { UploadImageService } from '../service/upload-image.service';
 import { UserService } from '../service/user.service';
 
@@ -9,76 +12,76 @@ import { UserService } from '../service/user.service';
   styleUrls: ['./question.component.css'],
 })
 export class QuestionComponent implements OnInit {
-  data: any;
-  answer = '';
-  answersList: any;
-  questionId: any;
-  warning = '';
+  public answerForm !: FormGroup;
+  questionData: QuestionType | undefined = undefined;
+  answersList: AnswerType[] = [];
+  questionId: number | undefined;
   uploadedImages: string[] = [];
-  file: File = new File(['init'], 'init.txt');
 
-  constructor(
-    private _userService: UserService,
-    private activatedRoute: ActivatedRoute,
-    private router: Router,
-    private _uploadService: UploadImageService
-  ) {}
+  constructor(private _userService: UserService, private activatedRoute: ActivatedRoute, private router: Router, private _uploadService: UploadImageService) {}
 
   ngOnInit(): void {
+    this.answerForm = new FormGroup({
+      answer: new FormControl('', [Validators.required]),
+      images: new FormControl('')
+    })
     this.getData();
   }
 
   getData() {
     let urlParams = this.activatedRoute.snapshot.params;
     this.questionId = urlParams['id'];
-    console.log(this.questionId);
-    this.data = this._userService.getQuestion(this.questionId).subscribe({
-      next: (res) => (this.data = res),
-      error: (err) => this.router.navigate(['/error/' + err.status]),
-    });
-
-    this._userService.getAnswers(this.questionId).subscribe({
-      next: (res) => (this.answersList = res),
-      error: (err) => this.router.navigate(['/error/' + err.status]),
-    });
+    if (this.questionId) {
+      this._userService.getQuestion(this.questionId).subscribe({
+        next: (result) => (this.questionData = result as QuestionType),
+        error: (err) => this.router.navigate(['/error/' + err.status]),
+      });
+      this._userService.getAnswers(this.questionId).subscribe({
+        next: (result) => (this.answersList = result as AnswerType[]),
+        error: (err) => this.router.navigate(['/error/' + err.status]),
+      });
+    }
   }
 
   onChange(event: any) {
-    this.file = event.target.files[0];
-  }
-  getImageUrl(image: any) {
-    const imageUrl = 'http://localhost:4000/' + image;
-    return imageUrl;
-  }
-  onUploadImage() {
-    this._uploadService.uploadImage(this.file).subscribe({
-      next: (res) => console.warn(res),
-      error: (err) => {
-        if (err.status === 200) this.uploadedImages.push(err.error.text);
-        else this.router.navigate(['/error/' + err.status]);
-      },
-    });
+    const imageFile = event.target.files[0];
+    if (imageFile) {
+      this._uploadService.uploadImage(imageFile).subscribe({
+        next: (result) => {
+          this.uploadedImages.push(result);
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error.status === 400) {
+            alert(error.error);
+          }
+          else {
+            this.router.navigate(['/error/' + error.status]);
+          }
+        },
+      });
+    }
   }
 
-  onSubmit() {
-    if (this.answer === '') {
-      this.warning = 'All fields are required!';
-      return;
-    }
-    this._userService
+  getImageUrl(imageName: string) {
+    return `${BASE_URL}/images/${imageName}`;
+  }
+
+  submit() {
+    if (this.questionId) {
+      this._userService
       .postAnswer(this.questionId, {
-        answer: this.answer,
+        answer: this.answerForm.value.answer,
         images: this.uploadedImages,
       })
       .subscribe({
-        next: (res) => {
-          alert('Successfully Submitted');
+        next: (result) => {
+          alert('Your answer submission was successful.');
           this.router.navigate(['/']);
         },
-        error: (err) => {
-          console.log(err);
-          this.router.navigate(['/error/' + err.status]);
+        error: (error) => {
+          this.router.navigate(['/error/' + error.status]);
         },
       });
+    }
   }
 }
