c11953e48cb4e3af6e17cafadecede5bee5d9ec3
Devashish Ashok Pathrabe <devashishpathrabe@gmail.com>
2022-08-30 13:31:07 +0530

    Update UserLoginRequestDTO.java

--- do_connect_backend/DoConnect/src/main/java/com/wipro/cp/doconnect/dto/UserLoginRequestDTO.java
+++ do_connect_backend/DoConnect/src/main/java/com/wipro/cp/doconnect/dto/UserLoginRequestDTO.java
@@ -1,3 +1,9 @@
+/*
+* @Author: Devashish Ashok Pathrabe
+* Modified Date: 26-08-2022
+* Description: User Login Request DTO
+*/
+
 package com.wipro.cp.doconnect.dto;
 
 import java.io.Serializable;
